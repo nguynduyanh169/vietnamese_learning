d4a66076401f2a0da1e63696319cf1ff37f59fd9
hoquangbao1@gmail.com <hoquangbao1@gmail.com>
2020-10-30 08:49:30 +0700

    baohq re-design sign up screen and game screeen

--- lib/src/resources/list_game_screen.dart
+++ lib/src/resources/list_game_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -7,109 +6,96 @@ import 'package:vietnamese_learning/src/config/size_config.dart';
 import 'package:vietnamese_learning/src/resources/game_screen.dart';
 import 'package:vietnamese_learning/src/resources/memory_game.dart';
 
-class ListGameScreen extends StatefulWidget{
-
-  ListGameScreen({Key key}) : super(key:key);
+class ListGameScreen extends StatefulWidget {
+  ListGameScreen({Key key}) : super(key: key);
 
   @override
   _ListGameScreenState createState() => _ListGameScreenState();
-
 }
 
-class _ListGameScreenState extends State<ListGameScreen>{
+class _ListGameScreenState extends State<ListGameScreen> {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     // TODO: implement build
     return Scaffold(
       body: Container(
-          child: Column(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(top: SizeConfig.blockSizeVertical * 7),
-                alignment: Alignment.center,
-                child: Column(
-                  children: <Widget>[
-                    Text('Game', style: GoogleFonts.sansita(
-                      fontSize: 20,
-                    ),),
-                    SizedBox(height: SizeConfig.blockSizeVertical* 5,),
-                    Container(
-                      width: SizeConfig.blockSizeHorizontal * 85,
-                      height: SizeConfig.blockSizeVertical * 30,
-                      child: InkWell(
-                        child: Card(
-                          color: Colors.green,
-                          elevation: 6,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15.0)),
-                          child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Padding(
-                                  padding: EdgeInsets.only(
-                                      top: SizeConfig.blockSizeVertical * 2),
-                                  child: Text(
-                                    'Matching game',
-                                    style: GoogleFonts.sansita(
-                                      textStyle: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ]),
-                        ),
-                        onTap: () => pushNewScreen(context,
-                            screen: GameScreen(),
-                            withNavBar: false,
-                            pageTransitionAnimation: PageTransitionAnimation.cupertino),
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(top: SizeConfig.blockSizeVertical * 7),
+              alignment: Alignment.center,
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    'Game',
+                    style: GoogleFonts.sansita(
+                        fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: SizeConfig.blockSizeVertical * 5,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/matchinggame.png"),
+                        fit: BoxFit.fill,
                       ),
-                    ),
-                    SizedBox(height: SizeConfig.blockSizeVertical * 5,),
-                    Container(
-                      width: SizeConfig.blockSizeHorizontal * 85,
-                      height: SizeConfig.blockSizeVertical * 30,
-                      child: InkWell(
-                        child: Card(
-                          color: Colors.amberAccent,
-                          elevation: 6,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15.0)),
-                          child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Padding(
-                                  padding: EdgeInsets.only(
-                                      top: SizeConfig.blockSizeVertical * 2),
-                                  child: Text(
-                                    'Card game',
-                                    style: GoogleFonts.sansita(
-                                      textStyle: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ]),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
                         ),
-                        onTap: () => pushNewScreen(context,
-                            screen: MemoryGamePage(),
-                            withNavBar: false,
-                            pageTransitionAnimation: PageTransitionAnimation.cupertino),
-                      ),
+                      ],
                     ),
-                  ],
-                ),
+                    width: SizeConfig.blockSizeHorizontal * 85,
+                    height: SizeConfig.blockSizeVertical * 30,
+                    child: InkWell(
+                      onTap: () => pushNewScreen(context,
+                          screen: GameScreen(),
+                          withNavBar: false,
+                          pageTransitionAnimation:
+                              PageTransitionAnimation.cupertino),
+                    ),
+                  ),
+                  SizedBox(
+                    height: SizeConfig.blockSizeVertical * 5,
+                  ),
+                  Container(
+                    width: SizeConfig.blockSizeHorizontal * 85,
+                    height: SizeConfig.blockSizeVertical * 30,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/matchingcard.png"),
+                        fit: BoxFit.fill,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: InkWell(
+                      onTap: () => pushNewScreen(context,
+                          screen: MemoryGamePage(),
+                          withNavBar: false,
+                          pageTransitionAnimation:
+                              PageTransitionAnimation.cupertino),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
+        ),
       ),
     );
   }
-
 }
